7d26506f164ec82719653c10f23e0dfd2cc7ffab
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2020-01-11 18:02:02 +0900

    12
    12

---
+++ 3.funtion2.py
@@ -0,0 +1,27 @@
+# def p_plus(a, b):
+#     print(a + b)
+# def r_plus(a, b):
+#     return a + b
+# p_result = p_plus(2, 3)
+# r_result = r_plus(2, 6)
+# print(p_result, r_result)
+
+
+# def plus(a, b):
+#     print(a + b)
+
+
+# result = plus(2, 6)
+# print(result)
+
+# def say_hello(name, age):
+#     return f"hello {name} you are {age} years old"
+
+# hello = say_hello("nico", "12")
+# print(hello)
+
+def say_hello(name, age):
+    return f"hello {name} you are {age} years old"
+
+hello = say_hello(age="12", name="nico")
+print(hello)
---
+++ 4.challenge.py
@@ -0,0 +1,33 @@
+# plus, minus, times, division, negation, power
+# 계산기 만들기
+
+
+def plus(a, b):
+    return print(a + b)
+
+
+def minus(a, b):
+    print(a - b)
+
+
+def times(a, b):
+    return print(a * b)
+
+
+def division(a, b):
+    return print(a / b)
+
+
+def negation(a):
+    return print(-a)
+
+
+def power(a, b):
+    return print(a ** b)
+
+
+def remainder(a, b):
+    return print(a % b)
+
+
+minus(2, 3)
